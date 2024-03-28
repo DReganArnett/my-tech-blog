927af8407f7aec4345796b364ff03e8d7598e5a8
DReganArnett <regandanielle54@gmail.com>
2024-03-28 10:51:46 -0600

    Created NavBar

---
+++ app/NavBar.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Link from 'next/link';
+import Image from 'next/image'
+import Logo from './assets/logo-laptop.png'
+
+const NavBar = () => {
+
+    const links = [
+        {label: 'Home', href: '/'},
+        {label: 'Posts', href: '/posts'},
+    ]
+
+  return (
+        <>
+            <nav> 
+                <ul className='flex space-x-6 border-b mb-5 mt-5 px-3 h-14'>
+                    <Link href='/'><Image src={Logo} alt='logo'/></Link>
+                    {links.map(link => 
+                        <Link  
+                            key={link.href}
+                            className='flex space-x-6 items-center text-zinc-500 hover:text-zinc-800'
+                            href={link.href}
+                            >
+                                {link.label}
+                        </Link> )}
+                </ul>
+            </nav>
+        </>
+        
+     
+
+  )
+}
+
+export default NavBar
